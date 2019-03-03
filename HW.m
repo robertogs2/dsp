72250d9228f97a3fd5224f8af90abd6b0acadac0
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2019-03-02 21:51:19 -0600

    Update HW.m

--- HW.m
+++ HW.m
@@ -1,19 +1,27 @@
 1;
 
-x = [1,1];
+x = [2,1,1,3];
 xi = -3;
 
-[xp,yi]=par_component(x, xi);
+[xp,xpi]=par_component(x, xi);
 
-function [xp, yi] = par_component(x, xi)
+%Determines the even component of a signal x starting at x
+%Result is stored in xp, and result start stored in xpi
+function [xp, xpi] = par_component(x, xi)
     xf = xi + length(x) - 1;
-    ri = -xf;%reverse index
+    ri = -xf;
     rf = -xi;
     xr = fliplr(x);
     
-    delta = abs(xf-rf);
-    xfilled = horzcat(zeros(1,delta),x);
-    xrfilled = horzcat(xr,zeros(1,delta));
-    xp = (xfilled+xrfilled)/2; 
-    yi = min(xi, ri);
+    delta = xf-rf %xf+xi
+    
+    if delta >= 0
+        xfilled = horzcat(zeros(1,delta),x);%Most common case: left zeros 
+        xrfilled = horzcat(xr,zeros(1,delta));%Most common case: right zeros
+    else
+        xfilled = horzcat(x, zeros(1,abs(delta)));%Less common case, anticausal signal
+        xrfilled = horzcat(zeros(1,abs(delta)), xr);%
+    end
+    xp = (xfilled+xrfilled)/2
+    xpi = min(xi, ri);
 end
