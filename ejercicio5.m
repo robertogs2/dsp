4bca429e150de2a1ddfadc242c4f873924b2344d
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2019-03-03 14:40:41 -0600

    5

---
+++ ejercicio5.m
@@ -0,0 +1,38 @@
+1;
+
+
+F0 = 500;
+zeta = 1;
+A = 10;
+Fs = 4000;
+Ts=1/Fs;
+ti = 0;
+tf = 10e-03;
+
+graph(A,F0,zeta,ti,tf,Ts)
+
+function [ts, ys_a, ns, ys_d] = graph_aux(A,F0,zeta,ti,tf,Ts)
+    %Analog
+    samples_inside = 100; %samples inside each Ts
+    ts = ti:Ts/samples_inside:tf; %Vector of ts
+    ys_a = A*sin(2*pi*F0*ts+zeta);
+    
+    f = F0*Ts;%f=F0/Fs
+    ni = fix(ti/Ts); %Forcing ni*Ts=ti, fix rounds towards 0
+    nf = fix(tf/Ts); %Forcing nf*Ts=tf
+    
+    ns = ni:1:nf;
+    ys_d = A*sin(2*pi*f*ns+zeta);
+end
+
+function a = graph(A,F0,zeta,ti,tf,Ts)
+    [ts, ys_a, ns, ys_d] = graph_aux(A,F0,zeta,ti,tf,Ts);
+    figure (1);
+    
+    p1 = plot(ts,ys_a);
+    hold on;
+    p2 = stem(Ts*ns,ys_d,'r','fill');%Multiply by Ts, cause we can't graph in time with that
+    hold off;
+    grid on;
+    legend("wasd", "asd");
+end
