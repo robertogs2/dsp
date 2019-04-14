ccc6474322b335441c02daab7032a73dba673369
Alejandro Calvo <ale_calvo03@outlook.com>
2019-04-13 23:10:04 -0600

    Cambio de nombre del archivo

--- Tarea2/Raiz.m
+++ Tarea2/Raiz.m
@@ -4,17 +4,16 @@
 % Tarea 2
 % Ejercicio 1
 % Abril de 2019
+
 % Integrantes: 
 %   Alejandro Calvo 2016138114
 %   Allan Gutiérrez 2016253395
 %   Roberto Gutiérrez 2016134351
-1;
-
-format long;
 
 %Funcion para resolver la ecuacion y = b^(1/a) con un
 %algoritmo iterativo implementado como un sistema en tiempo discreto
 function y = Raiz(a,b)
+    format long;
     precision = 0.00000001;		%Precision del valor de salida
     yp = 0;						%Valor anterior de y: y(n-1)
     y = 1;						%Valor actual de y: y(n)
