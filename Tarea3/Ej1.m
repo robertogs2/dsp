ae1e4791d2670c51a17810c895f947d093dd1438
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2019-05-19 11:45:30 -0600

    Comments

--- Tarea3/Ej1.m
+++ Tarea3/Ej1.m
@@ -1,11 +1,12 @@
-close all;
-w=-3*pi:0.001:3*pi;
+w=-3*pi:0.001:3*pi; %Frecuencia a evaluar
 
-[m1, p1] = arrayfun(@H1, w);
-[m2, p2] = arrayfun(@H2, w);
-[m3, p3] = arrayfun(@H3, w);
-%w=w/pi;
+%Evalua el vector de frecuencias en cada uno de las funciones y retorna vectores con
+%soluciones de mangitud y fase
+[m1, p1] = arrayfun(@H1, w); %Diagrama a
+[m2, p2] = arrayfun(@H2, w); %Diagrama b
+[m3, p3] = arrayfun(@H3, w); %Diagrama c
 
+%Grafica para diagrama a
 figure
 subplot(2,1,1)
 plot(w, m1)
@@ -19,6 +20,7 @@ title('Respuesta de fase a')
 xlabel('-3\pi < \omega < 3\pi') 
 ylabel('\angleH_a(\omega)') 
 
+%Grafica para diagrama b
 figure
 subplot(2,1,1)
 plot(w, m2)
@@ -32,6 +34,7 @@ title('Respuesta de fase b')
 xlabel('-3\pi < \omega < 3\pi') 
 ylabel('\angleH_b(\omega)') 
 
+%Grafica para diagrama c
 figure
 subplot(2,1,1)
 plot(w, m3)
@@ -45,18 +48,21 @@ title('Respuesta de fase c')
 xlabel('-3\pi < \omega < 3\pi') 
 ylabel('\angleH_c(\omega)') 
 
+%Evalua la formula para el diagrama a, tanto magnitud como fase
 function [m, p] = H1(w)
-    m=sqrt(2+2*cos(w))/2;
-    p=atan((-sin(w))/(cos(w)+1));
+    m=sqrt(2+2*cos(w))/2;%Magnitud
+    p=atan((-sin(w))/(cos(w)+1)); %Fase  
 end
 
+%Evalua la formula para el diagrama b, tanto magnitud como fase
 function [m, p] = H2(w)
-    m=sqrt(2-2*cos(w))/2;
-    p=atan((-sin(w))/(cos(w)-1))-pi;
+    m=sqrt(2-2*cos(w))/2; %Magnitud
+    p=atan((-sin(w))/(cos(w)-1))-pi; %Fase 
 end
 
+%Evalua la formula para el diagrama c, tanto magnitud como fase
 function [m, p] = H3(w)
-    [m, p] = H1(w);
-    m=m*m*m;
-    p=3*p;
+    [m, p] = H1(w); %Utiliza el resultado del diagrama a
+    m=m*m*m; %Eleva magnitud al cubo
+    p=3*p; %Triplica la fase
 end
--- Tarea3/Ej6.m
+++ Tarea3/Ej6.m
@@ -1,37 +1,45 @@
-%w0 frequency to generate
-
+%w0 Frecuencia a generar
 function Ej6(w0)
 
-    n=(2*pi/w0);
-    n=2*n;
-    n=floor(n+1);
+    %Calculo la cantidad de puntos a graficar
+    n=(2*pi/w0); %Despeje de w/2pi=F/Fs
+    n=2*n; %Ver dos periodos
+    n=floor(n+1); %Punto más para que cierre
+
+    %Calculo de constantes para el oscilador
     c = cos(w0);
     s = sin(w0);
+
+    %Inicialización de vectores
     yc = zeros(1,n);
     ys = zeros(1,n);
     
+    %Calculo condiciones iniciales
     ycc = c;
     yss = -s;
     
+    %Para todos los puntos, calcular el valor de las señales
     for i = 1:n
-        yc(i) = c*ycc-s*yss;
-        ys(i) = c*yss+s*ycc;
-        ycc = yc(i);
-        yss = ys(i);
+        yc(i) = c*ycc-s*yss; %Calculo según formula para el coseno
+        ys(i) = c*yss+s*ycc; %Calculo según formula para el seno
+        ycc = yc(i);    %Actualiza ultima valor del coseno
+        yss = ys(i);    %Actualiza ultima valor del seno
     end
 
+    %Graficacion de las señales
     figure
-    a=w0/pi;
+    a=w0/pi; %Variable para mostrar multiplos de pi en el titulo
     subplot(2,1,1);
-    plot(0:n-1,ys);
-    title(strcat('sin(',num2str(a),'\pin)'))
-    xlabel('0 \leq n < '+string(n)) 
+    plot(0:n-1,ys); %Variable va de 0 a n-1
+    title(strcat('sin(',num2str(a),'\pin)')) %Titulo con la frecuencia multiplo de pi
+    xlabel('0 \leq n < '+string(n))  %Cantidad de puntos 
     ylabel('Valores del seno') 
     
+    %Graficacion de las señales
     subplot(2,1,2);
-    plot(0:n-1,yc);
-    title(strcat('cos(',num2str(a),'\pin)')) 
-    xlabel('0 \leq n < '+string(n)) 
+    plot(0:n-1,yc); %Variable va de 0 a n-1
+    title(strcat('cos(',num2str(a),'\pin)')) %Titulo con la frecuencia multiplo de pi
+    xlabel('0 \leq n < '+string(n)) %Cantidad de puntos 
     ylabel('Valores del coseno') 
     
 end
