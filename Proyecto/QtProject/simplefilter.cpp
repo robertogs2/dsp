05263d68c7fbed30f2fd4eb0ba7c3b0fd60a1767
roberto <roberm98@outlook.com>
2019-06-15 01:38:04 -0600

    Simple filter addition

---
+++ Proyecto/QtProject/simplefilter.cpp
@@ -0,0 +1,6 @@
+#include "simplefilter.h"
+
+SimpleFilter::SimpleFilter()
+{
+
+}
---
+++ Proyecto/QtProject/simplefilter.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLEFILTER_H
+#define SIMPLEFILTER_H
+
+
+class SimpleFilter
+{
+public:
+    SimpleFilter();
+};
+
+#endif // SIMPLEFILTER_H
